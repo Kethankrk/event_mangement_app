1887aa3628826915ddecbf229bead54706b5824e
Kethankrk <kethankrk@gmail.com>
2024-09-17 22:37:22 +0530

    fix: added scroll on bottom keyboard inset

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -28,106 +28,118 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        height: double.infinity,
         width: double.infinity,
         decoration: const BoxDecoration(color: Color(0xff171717)),
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(40.0),
-            child: Column(
-              children: [
-                const SizedBox(height: 30.0),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Let's sign you in",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 42.0,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10.0),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Welcome back, \nYou've been missed!",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 50.0),
-                CustomInputField(
-                  label: "Email",
-                  controller: userNameController,
-                  prefixIcon: const Icon(
-                    Icons.person,
-                    color: Color.fromARGB(255, 97, 97, 97),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                CustomInputField(
-                  label: "Password",
-                  controller: passwordController,
-                  prefixIcon: const Icon(
-                    Icons.lock,
-                    color: Color.fromARGB(255, 97, 97, 97),
-                  ),
-                  obstruct: !_showPassword,
-                  suffixIconButton: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _showPassword = !_showPassword;
-                      });
-                    },
-                    icon: Icon(
-                      _showPassword ? Icons.visibility_off : Icons.visibility,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10.0),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Recover Password",
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                SizedBox(
-                  width: double.infinity,
-                  child: CustomButton(
-                    text: "Sign In",
-                    onPressed: handleSignIn,
-                  ),
-                ),
-                GoogleAuthOption(
-                  onPress: handleGoogleSignIn,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+            child: Center(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
                   children: [
-                    const Text(
-                      "Not a member?",
-                      style: TextStyle(color: Colors.white),
+                    const SizedBox(height: 30.0),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Let's sign you in",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 42.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
                     ),
-                    TextButton(
+                    const SizedBox(height: 10.0),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Welcome back, \nYou've been missed!",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 50.0),
+                    CustomInputField(
+                      label: "Email",
+                      controller: userNameController,
+                      prefixIcon: const Icon(
+                        Icons.person,
+                        color: Color.fromARGB(255, 97, 97, 97),
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    CustomInputField(
+                      label: "Password",
+                      controller: passwordController,
+                      prefixIcon: const Icon(
+                        Icons.lock,
+                        color: Color.fromARGB(255, 97, 97, 97),
+                      ),
+                      obstruct: !_showPassword,
+                      suffixIconButton: IconButton(
                         onPressed: () {
-                          Navigator.pushReplacementNamed(context, "/signup");
+                          setState(() {
+                            _showPassword = !_showPassword;
+                          });
                         },
+                        icon: Icon(
+                          _showPassword
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
                         child: const Text(
-                          "Register now",
+                          "Recover Password",
                           style: TextStyle(color: Colors.blue),
-                        ))
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    SizedBox(
+                      width: double.infinity,
+                      child: CustomButton(
+                        text: "Sign In",
+                        onPressed: handleSignIn,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 200,
+                      child: GoogleAuthOption(
+                        onPress: handleGoogleSignIn,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Not a member?",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(
+                                  context, "/signup");
+                            },
+                            child: const Text(
+                              "Register now",
+                              style: TextStyle(color: Colors.blue),
+                            ))
+                      ],
+                    ),
+                    const SizedBox(height: 40.0),
                   ],
                 ),
-                const SizedBox(height: 40.0),
-              ],
+              ),
             ),
           ),
         ),
--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -20,125 +20,138 @@ class _SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        height: double.infinity,
         width: double.infinity,
         decoration: const BoxDecoration(color: Color(0xff171717)),
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(40.0),
-            child: Column(
-              children: [
-                const SizedBox(height: 30.0),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Let's sign you up",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 42.0,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10.0),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Welcome to, \nPlanIt",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 50.0),
-                CustomInputField(
-                  label: "Email",
-                  controller: emailController,
-                  prefixIcon: const Icon(
-                    Icons.person,
-                    color: Color.fromARGB(255, 97, 97, 97),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                CustomInputField(
-                  label: "Password",
-                  controller: passwordController,
-                  prefixIcon: const Icon(
-                    Icons.lock,
-                    color: Color.fromARGB(255, 97, 97, 97),
-                  ),
-                  obstruct: !_showPassword,
-                  suffixIconButton: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _showPassword = !_showPassword;
-                      });
-                    },
-                    icon: Icon(
-                      _showPassword ? Icons.visibility_off : Icons.visibility,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                CustomInputField(
-                  label: "Confirm Password",
-                  controller: cfmPasswordController,
-                  prefixIcon: const Icon(
-                    Icons.lock,
-                    color: Color.fromARGB(255, 97, 97, 97),
-                  ),
-                  obstruct: !_showPassword,
-                  suffixIconButton: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _showPassword = !_showPassword;
-                      });
-                    },
-                    icon: Icon(
-                      _showPassword ? Icons.visibility_off : Icons.visibility,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10.0),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Recover Password",
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                SizedBox(
-                  width: double.infinity,
-                  child: CustomButton(
-                    text: "Sign Up",
-                    onPressed: () {},
-                  ),
-                ),
-                GoogleAuthOption(
-                  onPress: () {},
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+            child: Center(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
                   children: [
-                    const Text(
-                      "Already a member?",
-                      style: TextStyle(color: Colors.white),
+                    const SizedBox(height: 30.0),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Let's sign you up",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 42.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
                     ),
-                    TextButton(
+                    const SizedBox(height: 10.0),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Welcome to, \nPlanIt",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 50.0),
+                    CustomInputField(
+                      label: "Email",
+                      controller: emailController,
+                      prefixIcon: const Icon(
+                        Icons.person,
+                        color: Color.fromARGB(255, 97, 97, 97),
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    CustomInputField(
+                      label: "Password",
+                      controller: passwordController,
+                      prefixIcon: const Icon(
+                        Icons.lock,
+                        color: Color.fromARGB(255, 97, 97, 97),
+                      ),
+                      obstruct: !_showPassword,
+                      suffixIconButton: IconButton(
                         onPressed: () {
-                          Navigator.pushReplacementNamed(context, "/login");
+                          setState(() {
+                            _showPassword = !_showPassword;
+                          });
                         },
+                        icon: Icon(
+                          _showPassword
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    CustomInputField(
+                      label: "Confirm Password",
+                      controller: cfmPasswordController,
+                      prefixIcon: const Icon(
+                        Icons.lock,
+                        color: Color.fromARGB(255, 97, 97, 97),
+                      ),
+                      obstruct: !_showPassword,
+                      suffixIconButton: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _showPassword = !_showPassword;
+                          });
+                        },
+                        icon: Icon(
+                          _showPassword
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
                         child: const Text(
-                          "Login",
+                          "Recover Password",
                           style: TextStyle(color: Colors.blue),
-                        ))
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    SizedBox(
+                      width: double.infinity,
+                      child: CustomButton(
+                        text: "Sign Up",
+                        onPressed: () {},
+                      ),
+                    ),
+                    SizedBox(
+                      height: 200.0,
+                      child: GoogleAuthOption(
+                        onPress: () {},
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Already a member?",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(context, "/login");
+                            },
+                            child: const Text(
+                              "Login",
+                              style: TextStyle(color: Colors.blue),
+                            ))
+                      ],
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
