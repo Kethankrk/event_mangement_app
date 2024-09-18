7d8c84d535c4fb69a8ef5df03b95406180d4f685
Kethankrk <kethankrk@gmail.com>
2024-09-18 10:37:38 +0530

    feat: added error message and feedback animation

--- lib/pages/email_verification_page.dart
+++ lib/pages/email_verification_page.dart
@@ -1,10 +1,33 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:planit/utils/theme.dart';
 import 'package:planit/widgets/custom_button.dart';
 
-class EmailVerificationPage extends StatelessWidget {
+class EmailVerificationPage extends StatefulWidget {
   const EmailVerificationPage({super.key});
+
+  @override
+  State<EmailVerificationPage> createState() => _EmailVerificationPageState();
+}
+
+class _EmailVerificationPageState extends State<EmailVerificationPage> {
+  final StreamController<ErrorAnimationType> errorAnimationController =
+      StreamController<ErrorAnimationType>();
+  final TextEditingController otpController = TextEditingController();
+  bool hasError = false;
+  late String error;
+
+  void _handleEmailVerification() {
+    final String otp = otpController.text;
+    if (otp.length < 4) {
+      errorAnimationController.add(ErrorAnimationType.shake);
+      setState(() {
+        hasError = true;
+        error = "Please enter all 4 digits";
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +57,25 @@ class EmailVerificationPage extends StatelessWidget {
                   appContext: context,
                   length: 4,
                   textStyle: const TextStyle(color: Colors.white),
+                  keyboardType: TextInputType.number,
+                  animationType: AnimationType.scale,
+                  animationDuration: const Duration(milliseconds: 150),
+                  errorAnimationController: errorAnimationController,
+                  controller: otpController,
+                  onChanged: (v) {
+                    if (hasError) {
+                      setState(() {
+                        hasError = false;
+                      });
+                    }
+                  },
+                  onTap: () {
+                    if (hasError) {
+                      setState(() {
+                        hasError = false;
+                      });
+                    }
+                  },
                   pinTheme: PinTheme(
                     shape: PinCodeFieldShape.box,
                     borderRadius: BorderRadius.circular(10.0),
@@ -43,18 +85,22 @@ class EmailVerificationPage extends StatelessWidget {
                     inactiveColor: const Color.fromARGB(255, 97, 97, 97),
                   ),
                 ),
+                if (hasError)
+                  Text(
+                    error,
+                    style: errorTextTheme,
+                  ),
                 const SizedBox(height: 20.0),
                 SizedBox(
                   width: double.infinity,
-                  child: CustomButton(text: "Verify", onPressed: () {}),
+                  child: CustomButton(
+                      text: "Verify", onPressed: _handleEmailVerification),
                 ),
                 const SizedBox(height: 20.0),
                 Align(
                   alignment: Alignment.centerRight,
                   child: GestureDetector(
-                    onTap: () {
-                      print("lol");
-                    },
+                    onTap: () {},
                     child:
                         const Text("Resend email", style: hyperLinkTextTheme),
                   ),
