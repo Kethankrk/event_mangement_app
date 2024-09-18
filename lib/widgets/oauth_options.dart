0a2aed33e0e73e34446a838b57e98e630da58ded
Kethankrk <kethankrk@gmail.com>
2024-09-18 09:43:59 +0530

    refactor: removed unwanted expanded widget

--- lib/widgets/oauth_options.dart
+++ lib/widgets/oauth_options.dart
@@ -9,52 +9,50 @@ class GoogleAuthOption extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          const Row(
-            children: [
-              Expanded(
-                child: Divider(
-                  color: Colors.white,
-                  height: 4.0,
-                ),
-              ),
-              SizedBox(width: 10.0),
-              Text(
-                "OR",
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(width: 10.0),
-              Expanded(
-                child: Divider(
-                  color: Colors.white,
-                  height: 4.0,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: FilledButton(
-              onPressed: onPress,
-              style: FilledButton.styleFrom(
-                backgroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(vertical: 15.0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-              ),
-              child: const Text(
-                "Continue with google",
-                style: TextStyle(color: Colors.black),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        const Row(
+          children: [
+            Expanded(
+              child: Divider(
+                color: Colors.white,
+                height: 4.0,
               ),
             ),
+            SizedBox(width: 10.0),
+            Text(
+              "OR",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(width: 10.0),
+            Expanded(
+              child: Divider(
+                color: Colors.white,
+                height: 4.0,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: FilledButton(
+            onPressed: onPress,
+            style: FilledButton.styleFrom(
+              backgroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+            ),
+            child: const Text(
+              "Continue with google",
+              style: TextStyle(color: Colors.black),
+            ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
