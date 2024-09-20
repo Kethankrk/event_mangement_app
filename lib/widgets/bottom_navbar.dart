2f515740942b856055623463cb8b50874ead4ce9
Kethankrk <kethankrk@gmail.com>
2024-09-20 23:20:17 +0530

    feat: created bottom navbar

---
+++ lib/widgets/bottom_navbar.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:planit/providers/navbar_provider.dart';
+import 'package:planit/utils/theme.dart';
+import 'package:provider/provider.dart';
+
+class BottomNavbar extends StatelessWidget {
+  const BottomNavbar({super.key});
+
+  static const List<NavButton> navItems = [
+    NavButton(icon: Icons.home, name: "Home", path: "/"),
+    NavButton(icon: Icons.newspaper, name: "Updates", path: "/login"),
+    NavButton(icon: Icons.airplane_ticket, name: "Tickets", path: "/ticket"),
+    NavButton(icon: Icons.event, name: "Events", path: "/event-create"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      notchMargin: 0,
+      padding: const EdgeInsets.symmetric(horizontal: 40.0),
+      color: const Color(0xff28272A),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: List.generate(navItems.length, (index) {
+            return GestureDetector(
+              onTap: () {
+                context.read<NavbarProvider>().setActiveIndex(index);
+                Navigator.pop(context);
+                Navigator.pushNamed(context, navItems[index].path);
+              },
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    navItems[index].icon,
+                    color: index == context.watch<NavbarProvider>().acitveIndex
+                        ? Colors.blue
+                        : Colors.white,
+                  ),
+                  LabelSmall(
+                    text: navItems[index].name,
+                    color: index == context.watch<NavbarProvider>().acitveIndex
+                        ? Colors.blue
+                        : Colors.white,
+                  )
+                ],
+              ),
+            );
+          })),
+    );
+  }
+}
+
+class NavButton {
+  final IconData icon;
+  final String name;
+  final String path;
+  const NavButton({required this.icon, required this.name, required this.path});
+}
