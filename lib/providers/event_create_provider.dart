959b9d29cff3610e55ef6200fb4fa47a6cc26272
Kethankrk <kethankrk@gmail.com>
2024-09-20 22:12:25 +0530

    feat: added hero image state

--- lib/providers/event_create_provider.dart
+++ lib/providers/event_create_provider.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EventFormDataProvider extends ChangeNotifier {
   final List<String> _attendeesRequirements = [];
+  XFile? _heroImage;
 
   List<String> get req => _attendeesRequirements;
 
+  XFile? get heroImage => _heroImage;
+
   void addReq(String value) {
     _attendeesRequirements.add(value);
+    notifyListeners();
+  }
+
+  void addImage(XFile? image) {
+    _heroImage = image;
     notifyListeners();
   }
 
