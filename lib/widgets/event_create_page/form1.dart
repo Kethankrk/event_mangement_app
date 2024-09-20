4f1d91ce1c853e73fee90285bda550448d49ebfe
Kethankrk <kethankrk@gmail.com>
2024-09-20 22:12:55 +0530

    feat: added image upload option

--- lib/widgets/event_create_page/form1.dart
+++ lib/widgets/event_create_page/form1.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:planit/providers/event_create_provider.dart';
 import 'package:planit/utils/theme.dart';
+import 'package:planit/widgets/custom_button.dart';
 import 'package:planit/widgets/custom_input.dart';
 import 'package:provider/provider.dart';
 
@@ -12,6 +16,7 @@ class EventDetailsForm extends StatelessWidget {
     required this.titleController,
     required this.descriptionController,
   });
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -30,29 +35,97 @@ class EventDetailsForm extends StatelessWidget {
           minLines: 5,
         ),
         const SizedBox(height: 30.0),
+        if (context.watch<EventFormDataProvider>().heroImage == null)
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 1.0),
+            child: DottedBorder(
+              padding: const EdgeInsets.symmetric(vertical: 60.0),
+              color: Colors.white,
+              dashPattern: const [10, 8],
+              radius: const Radius.circular(10),
+              borderType: BorderType.RRect,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: IconButton(
+                      onPressed: () async {
+                        XFile? image = await _addImage(context);
+                        if (image == null) return;
+                        context.read<EventFormDataProvider>().addImage(image);
+                      },
+                      color: Colors.blue,
+                      iconSize: 42.0,
+                      icon: const Icon(
+                        Icons.upload_rounded,
+                      ),
+                    ),
+                  ),
+                  const DisplayMedium(
+                    text: "Upload Hero Photo",
+                    alignment: Alignment.center,
+                  ),
+                  const SizedBox(height: 10.0),
+                  const LabelSmall(
+                    text: "supports JPG, JPEG, PNG",
+                    alignment: Alignment.center,
+                  )
+                ],
+              ),
+            ),
+          ),
+        if (context.watch<EventFormDataProvider>().heroImage != null)
+          SizedBox(
+            height: 250.0,
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.file(
+                    File(
+                        context.watch<EventFormDataProvider>().heroImage!.path),
+                    width: double.infinity,
+                    height: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SizedBox(
+                      width: double.infinity,
+                      child: CustomButton(
+                          text: "Change Image",
+                          onPressed: () {
+                            context
+                                .read<EventFormDataProvider>()
+                                .addImage(null);
+                          })),
+                )
+              ],
+            ),
+          ),
+        const SizedBox(height: 30.0),
         const Align(
           alignment: Alignment.centerLeft,
           child: LabelLarge(text: "Attendees requirements"),
         ),
         const SizedBox(height: 5.0),
-        const AttendeesReqSection(),
+        AttendeesReqSection(),
       ],
     );
+  }
+
+  Future<XFile?> _addImage(BuildContext context) async {
+    return await ImagePicker().pickImage(source: ImageSource.gallery);
   }
 }
 
 // ##############################| Attendees requirement adding section |##############################
-class AttendeesReqSection extends StatefulWidget {
-  const AttendeesReqSection({
+class AttendeesReqSection extends StatelessWidget {
+  final TextEditingController controller = TextEditingController();
+  AttendeesReqSection({
     super.key,
   });
-
-  @override
-  State<AttendeesReqSection> createState() => _AttendeesReqSectionState();
-}
-
-class _AttendeesReqSectionState extends State<AttendeesReqSection> {
-  final TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -82,6 +155,7 @@ class _AttendeesReqSectionState extends State<AttendeesReqSection> {
         suffixIconButton: IconButton(
             color: Colors.blue,
             onPressed: () {
+              if (controller.text.isEmpty) return;
               context.read<EventFormDataProvider>().addReq(controller.text);
               controller.clear();
             },
