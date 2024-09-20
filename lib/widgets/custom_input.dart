35ddc0e862564ae80f80b69a75e133f3413ae57e
Kethankrk <kethankrk@gmail.com>
2024-09-20 14:29:01 +0530

    feat: added more options to custom input and created standard input

--- lib/widgets/custom_input.dart
+++ lib/widgets/custom_input.dart
@@ -3,19 +3,25 @@ import 'package:planit/utils/theme.dart';
 
 class CustomInputField extends StatelessWidget {
   final String label;
+  final String? hintText;
   final TextEditingController controller;
   final Icon? prefixIcon;
   final Icon? suffixIcon;
   final IconButton? suffixIconButton;
   final bool? obstruct;
+  final int? maxLines;
+  final int? minLines;
   const CustomInputField({
     super.key,
     required this.label,
     required this.controller,
+    this.hintText,
     this.prefixIcon,
     this.suffixIcon,
     this.suffixIconButton,
     this.obstruct,
+    this.maxLines = 1,
+    this.minLines,
   });
 
   @override
@@ -26,8 +32,11 @@ class CustomInputField extends StatelessWidget {
           color: Colors.white, fontWeight: FontWeight.w300, fontSize: 14),
       obscureText: obstruct ?? false,
       obscuringCharacter: "*",
+      maxLines: maxLines,
+      minLines: minLines,
       decoration: InputDecoration(
         label: LabelLarge(text: label),
+        hintText: hintText,
         floatingLabelStyle: const TextStyle(color: Colors.white),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10.0),
@@ -40,6 +49,63 @@ class CustomInputField extends StatelessWidget {
         prefixIcon: prefixIcon,
         suffixIcon: suffixIcon ?? suffixIconButton,
       ),
+    );
+  }
+}
+
+class StandardInputField extends StatelessWidget {
+  final String label;
+  final String? hintText;
+  final TextEditingController controller;
+  final Icon? prefixIcon;
+  final Icon? suffixIcon;
+  final IconButton? suffixIconButton;
+  final bool? obstruct;
+  final int? maxLines;
+  final int? minLines;
+  const StandardInputField({
+    super.key,
+    required this.label,
+    required this.controller,
+    this.hintText,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.suffixIconButton,
+    this.obstruct,
+    this.maxLines = 1,
+    this.minLines,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Align(alignment: Alignment.centerLeft, child: LabelLarge(text: label)),
+        TextField(
+          controller: controller,
+          style: const TextStyle(
+              color: Colors.white, fontWeight: FontWeight.w300, fontSize: 14),
+          obscureText: obstruct ?? false,
+          obscuringCharacter: "*",
+          maxLines: maxLines,
+          minLines: minLines,
+          decoration: InputDecoration(
+            hintText: hintText,
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide:
+                  const BorderSide(color: Color.fromARGB(255, 97, 97, 97)),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide:
+                  const BorderSide(color: Color.fromARGB(255, 97, 97, 97)),
+            ),
+            prefixIcon: prefixIcon,
+            suffixIcon: suffixIcon ?? suffixIconButton,
+          ),
+        ),
+      ],
     );
   }
 }
