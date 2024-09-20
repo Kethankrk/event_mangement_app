49fdc62304d64dbc04d29ebfeb91ed998787974f
Kethankrk <kethankrk@gmail.com>
2024-09-20 23:22:01 +0530

    feat: wrapped myapp with navbar provider

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:planit/pages/event_create_page.dart';
 import 'package:planit/pages/home_page.dart';
 import 'package:planit/pages/login_page.dart';
 import 'package:planit/pages/signup_page.dart';
+import 'package:planit/providers/navbar_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => NavbarProvider(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
