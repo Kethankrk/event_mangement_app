0442eb47ae9d4a2af6b5d0b4eec95ee8c5537774
Kethankrk <kethankrk@gmail.com>
2024-09-20 09:40:22 +0530

    feat: created bolierplate code for event creation page

---
+++ lib/pages/event_create_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventCreatePage extends StatefulWidget {
+  const EventCreatePage({super.key});
+
+  @override
+  State<EventCreatePage> createState() => _EventCreatePageState();
+}
+
+class _EventCreatePageState extends State<EventCreatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
