fe9f4b7ddb7ce22412d9b0485a3dfaa3b2f372b3
Kethankrk <kethankrk@gmail.com>
2024-09-21 19:18:13 +0530

    feat: added getter and setter for ticket data

--- lib/providers/event_create_provider.dart
+++ lib/providers/event_create_provider.dart
@@ -3,12 +3,15 @@ import 'package:image_picker/image_picker.dart';
 
 class EventFormDataProvider extends ChangeNotifier {
   final List<String> _attendeesRequirements = [];
+  final List<TicketData> _tickets = [];
   XFile? _heroImage;
 
+  // ----------------------| getters |------------------------
   List<String> get req => _attendeesRequirements;
-
   XFile? get heroImage => _heroImage;
+  List<TicketData> get tickets => _tickets;
 
+  // ----------------------| setters |------------------------
   void addReq(String value) {
     _attendeesRequirements.add(value);
     notifyListeners();
@@ -24,9 +27,28 @@ class EventFormDataProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  void addTicketData(TicketData ticket) {
+    _tickets.add(ticket);
+    notifyListeners();
+  }
+
   @override
   void dispose() {
     print("disposing provider");
     super.dispose();
   }
+}
+
+class TicketData {
+  String title;
+  String price;
+  String limit;
+  List<String> perks;
+
+  TicketData({
+    required this.title,
+    required this.price,
+    required this.limit,
+    required this.perks,
+  });
 }
