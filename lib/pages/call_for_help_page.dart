9801e16618b66b515e168f4c485e2e5d096c71b7
Kethankrk <kethankrk@gmail.com>
2024-11-18 19:08:13 +0530

    updated my events page

--- lib/pages/call_for_help_page.dart
+++ lib/pages/call_for_help_page.dart
@@ -2,39 +2,23 @@ import 'package:custom_sliding_segmented_control/custom_sliding_segmented_contro
 import 'package:flutter/material.dart';
 import 'package:planit/utils/theme.dart';
 import 'package:planit/widgets/base_layout.dart';
-import 'package:planit/widgets/homepage/appbar.dart';
-import 'package:planit/widgets/my_events_page/my_events_section.dart';
-import 'package:planit/widgets/my_events_page/participate_section.dart';
 
-class MyEventsPage extends StatefulWidget {
-  const MyEventsPage({super.key});
-
-  @override
-  State<MyEventsPage> createState() => _MyEventsPageState();
-}
-
-class _MyEventsPageState extends State<MyEventsPage> {
-  int active = 1;
+class CallForHelpPage extends StatelessWidget {
+  const CallForHelpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BaseLayout(
-      backgroundColor: Colors.black,
-      appbar: AppBar(
-        backgroundColor: Colors.black,
-        toolbarHeight: 90,
-        automaticallyImplyLeading: false,
-        leading: hompageAppBar,
-        leadingWidth: double.infinity,
-      ),
       child: Column(
         children: [
           CustomSlidingSegmentedControl<int>(
             isStretch: true,
             initialValue: 1,
             children: const {
-              1: CustomText(text: "Participating event"),
-              2: CustomText(text: "My events"),
+              1: CustomText(
+                text: "Call for Volunteers",
+              ),
+              2: CustomText(text: "Call for Speakers"),
             },
             decoration: BoxDecoration(
               color: Colors.black,
@@ -57,19 +41,8 @@ class _MyEventsPageState extends State<MyEventsPage> {
             ),
             duration: const Duration(milliseconds: 300),
             curve: Curves.easeInToLinear,
-            onValueChanged: (v) {
-              setState(() {
-                active = v;
-              });
-            },
+            onValueChanged: (v) {},
           ),
-          const SizedBox(height: 20),
-          active == 1
-              ? SizedBox(
-                  height: MediaQuery.of(context).size.height * .56,
-                  child: const ParticipateSection(),
-                )
-              : const MyEventsSection()
         ],
       ),
     );
