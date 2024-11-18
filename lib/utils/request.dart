58543fe968438cb7d8da0ad51d2aa6b0f4c76ef4
Kethankrk <kethankrk@gmail.com>
2024-11-18 19:35:56 +0530

    updated request url

--- lib/utils/request.dart
+++ lib/utils/request.dart
@@ -4,7 +4,7 @@ Future<http.Response?> getHelper(String url) async {
   try {
     const token =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MX0.u2tVDnt_IxA1C3De2peypdja96HtAGzwwNT7AkvhgrU";
-    return await http.get(Uri.parse("http://192.168.29.20:8000$url"),
+    return await http.get(Uri.parse("https://plan-it-backend.vercel.app$url"),
         headers: {'Authorization': 'Bearer $token'});
   } catch (e) {
     print(e);
